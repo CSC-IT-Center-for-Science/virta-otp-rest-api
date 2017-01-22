7d11ca1ae8ce507966d04e63e9c410056022087b
Fredrik Finnberg <fredrikfinnberg@users.noreply.github.com>
2017-01-22 12:36:55 +0200

    Add files via upload
    Avoimet ja erilliset testidataa

---
+++ src/test/resources/sql/avoimet_erilliset_yo_test.sql
@@ -0,0 +1,7 @@
+INSERT INTO Avoimet_Erilliset_YO (id, avomiehet, avonaiset, avotutkintomiehet, avotutkintonaiset, erillisetoikeudet, erillisetopettajat, kk, organisaatiokoodi, vuosi) 
+							VALUES  ( 1, 11, 11, 11, 11, 11, 11, 'aa', '12345', 2016 );
+INSERT INTO Avoimet_Erilliset_YO (id, avomiehet, avonaiset, avotutkintomiehet, avotutkintonaiset, erillisetoikeudet, erillisetopettajat, kk, organisaatiokoodi, vuosi)    
+							VALUES  ( 2, 22, 22, 22, 22, 22, 22, 'bb', '23456', 2016 );
+INSERT INTO Avoimet_Erilliset_YO (id, avomiehet, avonaiset, avotutkintomiehet, avotutkintonaiset, erillisetoikeudet, erillisetopettajat, kk, organisaatiokoodi, vuosi)  
+							VALUES  ( 3, 33, 33, 33, 33, 33, 33, 'cc', '34567', 2016 );
+  
