c1a90cf842c084a43754fc4a8f69bbf9f816f48a
fredrikfinnberg <fredrik.finnberg@gmail.com>
2018-05-31 12:15:26 +0300

    korjaus pvm

--- src/test/resources/sql/erikoistumiskoulutukset_test.sql
+++ src/test/resources/sql/erikoistumiskoulutukset_test.sql
@@ -1,6 +1,6 @@
 INSERT INTO erikoistumiskoulutukset (id, erikoistumiskoulutuskoodi, ika, ohjauksenalakoodi, opiskeluoikeudenalkamispaivamaara, oppilaitoskoodi,sukupuolikoodi) 
 							VALUES  ( 1,'048',60,'8','2017-09-22','02509','1' );
 INSERT INTO erikoistumiskoulutukset (id, erikoistumiskoulutuskoodi, ika,ohjauksenalakoodi, opiskeluoikeudenalkamispaivamaara, oppilaitoskoodi, sukupuolikoodi)    
-							VALUES  ( 2,'024',18,'2','2017-01-16','02535','2');
+							VALUES  ( 2,'024',18,'2','2017-09-22','02535','2');
 
   
